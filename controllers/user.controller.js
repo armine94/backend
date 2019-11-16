9df8adf74a4cb3020b0d992cf90be5e939124d51
Armine <arm.gevorgyan94@gmail.com>
2019-11-16 21:06:04 +0400

    -Modified route.js and user.controller.js

---
+++ controllers/user.controller.js
@@ -0,0 +1,101 @@
+const validateRegisterInput = require('../validation/register');
+const validateLoginInput = require('../validation/login');
+const settings = require('../configs/envSettings.json');
+const User = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+const log4js = require('log4js');
+const logger = log4js.getLogger('logger');
+
+addUser = function(req, res) {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+    User.findOne({
+        email: req.body.email
+    }).then(user => {
+        if(user) {
+            logger.error("Email already exists");
+            return res.status(400).json({
+                email: 'Email already exists'
+            });
+        }
+        else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                if(err) logger.error('There was an error', err);
+                else {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) logger.error('There was an error', err);
+                        else {
+                            newUser.password = hash;
+                            newUser
+                            .save()
+                            .then(user => {
+                                logger.info('User already added');
+                                res.json(user)
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+
+const loginUser = function (req, res) {
+    const { errors, isValid } = validateLoginInput(req.body);
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email})
+    .then(user => {
+        if(!user) {
+            errors.email = 'User not found'
+            return res.status(404).json(errors);
+        }
+        bcrypt.compare(password, user.password)
+        .then(isMatch => {
+            if (isMatch) {
+                logger.info('Login successful');
+                res.json({
+                    success: true,
+                    email: user.email
+                });
+            }
+            else {
+                errors.password = 'Incorrect Password';
+                return res.status(400).json(errors);
+            }
+        });
+    });
+}
+
+const logoutUser = function (req, res) {
+    if (req.session) {
+        req.session.destroy(error => {
+            req.session = null;
+            if (error) {
+                logger.error(error);
+                return next(error);
+            }
+            res.clearCookie(settings.session.key);
+            logger.info('Logout successful');
+            res.status(200).send("Ok");
+        });
+    }
+}
+
+module.exports.addUser = addUser;
+module.exports.loginUser = loginUser;
+module.exports.logoutUser = logoutUser;
--- routes/route.js
+++ routes/route.js
@@ -1,69 +1,20 @@
-const log4js = require('log4js');
-const express = require('express');
-const logger = log4js.getLogger('logger');
 const user = require('../controlers/user.controler');
 const image = require('../controlers/image.controler');
 const audio = require('../controlers/audio.controler');
-const settings = require('../configs/envSettings.json');
-const validateLoginInput = require('../validation/login');
-const validateRegisterInput = require('../validation/register');
+const express = require('express');
 const router = express.Router();
 
 //=========================== User Route ================================
 router.post('/users/register', async function (req, res) {
-    const { errors, isValid } = validateRegisterInput(req.body);
-    if (!isValid) {
-        logger.error(errors);
-        return res.status(400).json(errors);
-    }
-
-    const newUser = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }
-    const response = await user.addUser(newUser)
-
-    logger.info(response);
-    if (response.status === 200) {
-
-        logger.info(response);
-        res.status(200).end('Ok')
-    } else {
-        logger.error(response);
-        res.status(400).end(response.error);
-    }
+    user.addUser(req, res)
 });
 
 router.post('/users/login', async (req, res) => {
-    const { errors, isValid } = validateLoginInput(req.body);
-    if (!isValid) {
-        logger.error(errors);
-        return res.status(400).json(errors);
-    }
-
-    const email = req.body.email;
-    const password = req.body.password;
-    const response = await user.loginUser(email, password)
-    if (response.status === 200) {
-        logger.info(response);
-        res.send(response);
-    } else {
-        logger.error(response);
-        res.status(400).end('Incorrect email or password');
-    }
+    user.loginUser(req, res);
 });
 
 router.post('/users/logout', (req, res) => {
-    if (req.session) {
-        req.session.destroy(error => {
-            req.session = null;
-            if (error) return next(error);
-            user.logoutUser(req.body.email);
-            res.clearCookie(settings.session.key);
-            res.status(200).send("Ok");
-        });
-    }
+    user.logoutUser(req, res);
 });
 
 //============================ Image Route ============================================
